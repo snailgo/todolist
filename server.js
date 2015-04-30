4c46e7967c74d567a779e410a1bdd38febb913de
Pengfei Wu <pengfei.wu@newfieldwireless.com>
2015-04-30 11:29:49 -0700

    [Develop]: Add model and controller

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var app       = express();  // create out app w/ express
 var mongoose  = require('mongoose');  // mongoose for mongodb
 var morgan    = require('morgan');  // log request to the console
 var bodyParser  = require('body-parser'); // pull information from HTML POST
-var methodOverride = require('method-override'); // simulate DELETE and PUT 
+var methodOverride = require('method-override'); // simulate DELETE and PUT
 
 
 // configuration =================
@@ -21,46 +21,68 @@ app.use(bodyParser.json());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
 app.use(methodOverride());
 
-app.listen(8080);
-console.log("App is listening on port 8080");
-
 
 // define model ==================
-var Todo = mongoose.model('Todo', {
-    test : String   
+var Todo = mongoose.model('todo', {
+    test : String
 });
 
 
-// RESTful API ===================
+// routes ===========================================================================
+
+  // api ----------------------------------------------------------------------------
 
   // get all todos
   app.get('/api/todos', function(req, res) {
-    
+
     //use mongoose to get all todos in the database
     Todo.find(function(err, todos){
-      
+
       if (err)
         res.send(err);
-      
-      res.josn(todos);  //return all todos in JSON format   
+
+      res.josn(todos);  //return all todos in JSON format
     });
   });
-  
+
   app.post('/api/todos', function(req, res){
-  
+
     Todo.create({
       text : req.body.test,
       done : false
     }, function(err, todo) {
         if(err)
           res.send(err);
-          
+
         Todo.find(function(err, todos) {
           if (err)
             res.send(err)
-            
+
           res.json(todos);
         });
     });
   });
 
+  app.delete('/api/todos/:todo_id', function(req, res){
+    Todo.remove({
+        _id : req.params.todo_id
+    }, function(err, todo) {
+        if (err)
+            res.send(err);
+
+        Todo.find(function(err, todos) {
+            if (err)
+                res.send(err);
+            res.json(todos);
+        });
+    });
+  });
+
+
+
+app.get('*', function(req, res) {
+    res.sendfile('./public/index.html');
+});
+
+app.listen(8080);
+console.log("App is listening on port 8080");
